4150c1f41da855806b918461855dda20f0f2ac44
Deep Rahman <dp.rahman@gmail.com>
2019-04-13 13:28:14 +0600

    gulpfile watch completed

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 let gulp = require('gulp');
 let sass = require('gulp-sass');
-
+let watch = require('gulp-watch');
 //Run SASS compiler
 gulp.task('sass', ()=>{
     return gulp.src('sass/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('.'));
+});
+
+//Run the sass watcher
+gulp.task('watch', function(){
+    return watch('./sass/**/*.scss',function() {
+        gulp.src('sass/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('.'));
+    });
 });
